b349bd4e0fdd15ebefe7a30e6c55b5714f72568e
George <90505841+Geeeorge@users.noreply.github.com>
2022-10-31 17:35:45 +0000

    Add files via upload

---
+++ Main/src/MainFrame.java
@@ -0,0 +1,11 @@
+import javax.swing.*;  
+  
+class MainFrame extends JFrame  
+{  
+    MainFrame()  
+    {  
+        setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);  
+        setTitle("Test");  
+        setSize(500, 350);  
+    }  
+}  
